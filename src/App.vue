ee5f331c0aaa7b38b8ac79e506380e339b7e7c15
scottyjoppy <alexanderdelisle@yahoo.ca>
2025-10-28 14:01:00 +0100

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { supabase } from "@/lib/subabaseClient";
+import { onMounted, ref } from "vue";
+
+const sports = ref([]);
+
+async function getInstruments() {
+  const { data } = await supabase.from("sports").select();
+  sports.value = data;
+}
+
+onMounted(() => {
+  getInstruments();
+});
+</script>
+
+<template>
+  <ul>
+    <li class="hover:font-bold hover:cursor-default highlight-red" v-for="sport in sports" :key="sport.id">
+      {{ sport.name }}
+    </li>
+  </ul>
+</template>
